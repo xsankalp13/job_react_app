642f4b4edce3681cb17c871234150d865f31c952
xsankalp13 <sankalpbsalve@gmail.com>
2023-03-21 18:36:02 +0530

    I have added the basic structure of the app and Im signing off time==6:35 In india

---
+++ app/_layout.js
@@ -0,0 +1,7 @@
+import { Stack } from "expo-router";
+import { Layout } from "react-native-reanimated";
+
+const layout = () =>{
+    return <Stack/>;
+}
+export default Layout;
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Home = () =>{
+  retrun(
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home;
+
